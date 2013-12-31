32bf6b3acdde8cad37553199523e728c4b381832
shalinshah1993 <shalinshah1993@gmail.com>
2014-01-01 05:03:45 +0530

    Changed ViewDetailPage Look

--- Health Organizer/Health Organizer/AnalysisPage.xaml.cs
+++ Health Organizer/Health Organizer/AnalysisPage.xaml.cs
@@ -1122,7 +1122,7 @@ namespace Health_Organizer
         private void checkSelectedDates()
         {
             string FROM = AnalysisFromDayComboBox.SelectedItem.ToString() + "-" + (AnalysisFromMonthComboBox.SelectedIndex + 1).ToString() + "-" + AnalysisFromYearComboBox.SelectedItem.ToString();
-            string pattern = "dd-MM-yyyy";
+            string pattern = "dd-M-yyyy";
             DateTime from;
 
             if (!DateTime.TryParseExact(FROM, pattern, null, System.Globalization.DateTimeStyles.None, out from))
